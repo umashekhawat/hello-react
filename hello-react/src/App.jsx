ccaf3d9be387ee6a216f6426587504844f66d6f0
umashekhawat <umashekhawatbca19@gmail.com>
2026-08-12 17:11:39 +0530

    Hello React practical

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App 🚀</h1>
+      <h2>Hello, React!</h2>
+      <p>This is my first React practical.</p>
+
+      <button>Explore React</button>
+    </div>
+  );
+}
+
+export default App;
